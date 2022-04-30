2db077eb63c6794d0b20bfc04bdeb8ead0ba2c5f
Ashley's PC <ashlynn4567@gmail.com>
2022-04-30 15:20:17 -0500

    added api routes to allow get requests for either one or all parties from parties table, also added delete api route for parties table

--- server.js
+++ server.js
@@ -112,7 +112,60 @@ app.post("/api/candidate", ({ body }, res) => {
     });
 });
 
+// get all parties
+app.get("/api/parties", (req, res) => {
+    const sql = `SELECT * FROM parties`;
 
+    db.query(sql, (err, rows) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+            return;
+        };
+        res.json({
+            message: "success", 
+            data: rows
+        });
+    });
+});
+
+// get a single party
+app.get("/api/party/:id", (req, res) => {
+    const sql = `SELECT * FROM parties WHERE id = ?`;
+    const params =[req.params.id];
+
+    db.query(sql, params, (err, row) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        };
+        res.json({
+            message: "success",
+            data: row
+        });
+    });
+});
+
+// delete a party
+app.delete("/api/party/:id", (req, res) => {
+    const sql = `DELETE FROM parties WHERE id = ?`;
+    const params = [req.params.id];
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        } else if (!result.affectedRows) {
+            res.json({
+                message: "Party not found."
+            });
+        } else {
+            res.json({
+                message: "deleted",
+                changes: result.affectedRows, 
+                id: req.params.id
+            });
+        };
+    });
+});
 
 // default response for any other request (not found)
 app.use((req, res) => {
