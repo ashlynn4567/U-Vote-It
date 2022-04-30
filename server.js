175a2a87bd4621c22e8a06ded21a23cec929fb28
Ashley's PC <ashlynn4567@gmail.com>
2022-04-30 13:39:46 -0500

    implemented db connection by adding query parameters

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mysql = require("mysql2");
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,11 +8,51 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Hello World!"
-    });
-});
+// connect to database 
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // your mysql username
+        user: "root",
+        // your mysql password
+        password: "",
+        database: "election"
+    },
+    console.log("Connected to the election database")
+);
+
+// // get all candidates
+// db.query(`SELECT * FROM candidates`, (err, rows) => {
+//     console.log(rows);
+// });
+
+// // get a single candidate
+// db.query(`SELECT * FROM candidates WHERE id = 1`, (err, row) => {
+//     if (err) {
+//         console.log(err);
+//     };
+//     console.log(row);
+// });
+
+// // delete a candidate
+// db.query(`DELETE FROM candidates WHERE id = ?`, 1, (err, result) => {
+//     if (err) {
+//         console.log(err);
+//     };
+//     console.log(result);
+// });
+
+// // create a candidate
+// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
+//                 VALUES (?, ?, ?, ?)`;
+// const params = [1, "Ronald", "Firbank", 1];
+
+// db.query(sql, params, (err, result) => {
+//     if(err) {
+//         console.log(err);
+//     };
+//     console.log(result);
+// });
 
 // default response for any other request (not found)
 app.use((req, res) => {
