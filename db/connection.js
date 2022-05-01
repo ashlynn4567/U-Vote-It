913b3ffbfea2a6af3968b9ae0f74e7d5ce02427f
Ashley's PC <ashlynn4567@gmail.com>
2022-04-30 23:00:20 -0500

    created separation of concerns by separating all routes into individual files and folders

---
+++ db/connection.js
@@ -0,0 +1,19 @@
+// import all necessary dependencies
+const mysql = require("mysql2");
+
+
+// connect to database 
+const db = mysql.createConnection(
+    {
+        host: "localhost",
+        // your mysql username
+        user: "root",
+        // your mysql password
+        password: "",
+        database: "election"
+    },
+    console.log("Connected to the election database")
+);
+
+
+module.exports = db;
