c33a358cbeef6e9832f71ce829de9095c179dd5c
Ashley's PC <ashlynn4567@gmail.com>
2022-06-16 13:46:03 -0500

    connected sql database to heroku

--- db/connection.js
+++ db/connection.js
@@ -4,7 +4,12 @@ const mysql = require("mysql2");
 // connect to database 
 const db = mysql.createConnection(
     {
-        port: process.env.PORT || 3001
+        host: "us-cdbr-east-05.cleardb.net",
+        // your mysql username
+        user: "bf8306e143c5c3",
+        // your mysql password
+        password: "f5b24d4a",
+        database: "heroku_ae8597f4fa4d8ed"
     },
     console.log("Connected to the election database")
 );
