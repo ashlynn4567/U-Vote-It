3347b7abc1f9dc5ded85f0dd9ff5254edd995a9e
Ashley's PC <ashlynn4567@gmail.com>
2022-06-16 13:26:30 -0500

    bug fixes

--- db/connection.js
+++ db/connection.js
@@ -4,12 +4,7 @@ const mysql = require("mysql2");
 // connect to database 
 const db = mysql.createConnection(
     {
-        host: "localhost" || "0.0.0.0",
-        // your mysql username
-        user: "root",
-        // your mysql password
-        password: "",
-        database: "election"
+        port: process.env.PORT || 3001
     },
     console.log("Connected to the election database")
 );
